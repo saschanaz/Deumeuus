ffaff170f3c427652c243531a556cbb0a768ed1e
Kagami Sascha Rosylight <saschanaz@outlook.com>
2018-09-05 21:52:47 +0900

    skeleton for streaming support

---
+++ Deumeuus/sources/apis/streaming.ts
@@ -0,0 +1,26 @@
+﻿import { Notification, Status } from "../entities";
+
+export type MastodonStreamingContent = {
+  event: "update";
+  data: Status;
+} & {
+  event: "notification";
+  data: Notification;
+} & {
+  event: "delete";
+  data: string;
+} & {
+  event: "filters_changed";
+}
+
+function eventSourceToAsyncIterator() {
+
+}
+
+export class MastodonStreamingAPI {
+  constructor(public instanceURL: string, private userAccessToken: string) { }
+
+  user() {
+    
+  }
+}
