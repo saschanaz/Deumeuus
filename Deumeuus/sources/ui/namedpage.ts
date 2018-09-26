7ceb9405dcf2a0f9343674c5c37542c6262ce188
Kagami Sascha Rosylight <saschanaz@outlook.com>
2018-09-26 22:24:23 +0900

    namedpage

---
+++ Deumeuus/sources/ui/namedpage.ts
@@ -0,0 +1,43 @@
+﻿import { element } from "domliner";
+
+interface NamedPageInternalStates {
+  title: string | null;
+
+  elements: {
+    titleBar: HTMLDivElement;
+    contentElement: HTMLDivElement;
+  } | null;
+}
+
+export default class NamedPage extends HTMLElement {
+  private _states: NamedPageInternalStates = {
+    title: null,
+    elements: null
+  };
+
+  get pageTitle() {
+    return this._states.title;
+  }
+  set pageTitle(text: string | null) {
+    this._states.title = text;
+
+    this._states.elements!.titleBar.textContent = text || "";
+  }
+
+  constructor(title?: string) {
+    super();
+    this._initializeDOM();
+    if (title) {
+      this.pageTitle = title;
+    }
+  }
+
+  private _initializeDOM() {
+    const elements = this._states.elements! = ({} as NamedPageInternalStates["elements"])!;
+    element(this, undefined, [
+      elements.titleBar = element("div", { class: "namedpage-titlebar backgroundaccent" }),
+      elements.contentElement = element("div", { class: "flexfill verticalscrollable" })
+    ]);
+  }
+}
+customElements.define("due-titledholder", NamedPage);
