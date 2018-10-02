39dc643c6ab79c769ef9642b8cd4bcb31043ca8f
Kagami Sascha Rosylight <saschanaz@outlook.com>
2018-10-02 16:07:35 +0900

    use namedpage for conversation dialog

--- Deumeuus/sources/dialog-openers.ts
+++ Deumeuus/sources/dialog-openers.ts
@@ -2,10 +2,16 @@ import { MastodonAPI } from "./api";
 import openDialog from "./dialog-open";
 import { Status } from "./entities";
 import ConversationViewer from "./ui/conversation-viewer";
+import NamedPage from "./ui/namedpage";
 
 export function openConversationPopup(user: MastodonAPI, status: Status) {
   openDialog({
-    nodes: [new ConversationViewer({ user, status })],
+    nodes: [
+      new NamedPage({
+        pageTitle: "Conversations",
+        content: new ConversationViewer({ user, status })
+      })
+    ],
     classes: ["limitedwidth"]
   });
 }
--- Deumeuus/sources/ui/namedpage.ts
+++ Deumeuus/sources/ui/namedpage.ts
@@ -1,17 +1,20 @@
 import { element } from "domliner";
 
-interface NamedPageInternalStates {
+interface NamedPageInternalStates<T> {
   title: string | null;
+  content: T | null;
 
   elements: {
     titleBar: HTMLDivElement;
-    contentElement: HTMLDivElement;
+    container: HTMLDivElement;
   };
 }
 
-export default class NamedPage extends HTMLElement {
-  private readonly _states: NamedPageInternalStates = {
+export default class NamedPage<T extends HTMLElement> extends HTMLElement {
+  private readonly _states: NamedPageInternalStates<T> = {
     title: null,
+    content: null,
+
     elements: this._initializeDOM()
   };
 
@@ -24,20 +27,37 @@ export default class NamedPage extends HTMLElement {
     this._states.elements.titleBar.textContent = text || "";
   }
 
-  constructor(title?: string) {
+  get content() {
+    return this._states.content;
+  }
+  set content(value: T | null) {
+    const { content, elements } = this._states;
+    if (content && content !== value) {
+      content.remove();
+    }
+    if (value) {
+      elements.container.appendChild(value);
+    }
+    this._states.content = value;
+  }
+
+  constructor({ pageTitle, content }: { pageTitle?: string, content?: T }) {
     super();
-    if (title) {
-      this.pageTitle = title;
+    if (pageTitle) {
+      this.pageTitle = pageTitle;
+    }
+    if (content) {
+      this.content = content;
     }
   }
 
   private _initializeDOM() {
-    const elements = {} as NamedPageInternalStates["elements"];
+    const elements = {} as NamedPageInternalStates<T>["elements"];
     element(this, undefined, [
       elements.titleBar = element("div", { class: "namedpage-titlebar backgroundaccent" }),
-      elements.contentElement = element("div", { class: "flexfill verticalscrollable" })
+      elements.container = element("div", { class: "flexfill verticalscrollable" })
     ]);
     return elements;
   }
 }
-customElements.define("due-titledholder", NamedPage);
+customElements.define("deu-namedpage", NamedPage);
